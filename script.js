2f3c43888ae1e6579656f9a9ef65ff067a838028
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-14 22:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-	return (num-1)+ (num-2);
+	if( num == 0) return 0;
+	if( num == 1) return 1;
+
+	let a= 0, b = 1;
+	for(let i = 2; i <= num; i++){
+		let temp = a + b;
+		a = b ;
+		b = temp;
+	}
+	return b;
 // your code here
 }
 
